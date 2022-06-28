7791015fdd480592232260f68af54eb14f3b3d17
Guido Sabik <guido.sabik@gmail.com>
2022-06-28 18:58:44 -0300

    clean code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
             <div className="container m-4">
                 <div className="h-full flex flex-col space-y-12">
                     <h2 className="text-center text-4xl font-bold" data-aos="fade" data-aos-once="true">Contacto</h2>
-                    <p className="text-center text-lg" data-aos="fade" data-aos-delay="250" data-aos-once="true">Actualmente me encuentro en busca de mi primer empleo IT, y si estas interesad@ en contactarme te dejo los siguiente medios!</p>
+                    <p className="text-center text-lg" data-aos="fade" data-aos-delay="250" data-aos-once="true">Actualmente me encuentro en busca de mi primer empleo IT, y si estas interesad@ en contactarme te dejo los siguientes medios!</p>
                     <div className="w-full flex flex-row justify-center space-x-8" data-aos="fade" data-aos-delay="500" data-aos-once="true">
                         <a className="flex flex-row items-center font-medium space-x-2" href="mailto:guidosabik1@gmail.com" rel="noreferrer" target="_blank">
                             <img
