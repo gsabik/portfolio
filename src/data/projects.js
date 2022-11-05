49aeca495304ef5fb24c3c7e96c1e03ac2c4e0af
Guido Sabik <guido.sabik@gmail.com>
2022-11-05 19:18:30 -0300

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+	{
+		title: "Lisa Showroom",
+		description: "eCommerce de zapatillas de básquet. No hay mucha variedad, pero si buenos precios",
+		technologies: "Create React App, React JS, React Router, Chakra UI y Firebase.",
+		github: "https://github.com/gsabik/lisa-showroom",
+		visit: "https://lisa-showroom.vercel.app/",
+	},
+	{
+		title: "Journal App",
+		description: "Aplicación de notas, con manejo de estado en Redux Toolkit y autenticación. ¿Un extra? Podés subir imágenes.",
+		technologies: "Vite, React JS, React Router, Material UI, Redux Toolkit, Firebase y Cloudinary.",
+		github: "https://github.com/gsabik/journal-app",
+		visit: "https://journal-auth-app.vercel.app",
+	},
+	{
+		title: "OpenWeather API",
+		description: "¿Cómo va a estar el clima hoy o dentro de los próximos días? La verdad no sé, pero si entras acá te vas a enterar.",
+		technologies: "Vite, React JS, React Router, Chakra UI y OpenWeather API.",
+		github: "https://github.com/gsabik/openweather-api",
+		visit: "https://openweather-api-app.vercel.app",
+	}
+];
