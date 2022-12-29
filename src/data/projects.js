9f5e45ea6ab8ea59a842d0277b73f405de2c39fb
Guido Sabik <guido.sabik@gmail.com>
2022-12-29 18:26:38 -0300

    add new project data

--- src/data/projects.js
+++ src/data/projects.js
@@ -14,6 +14,13 @@ export const projects = [
 		visit: "https://journal-auth-app.vercel.app",
 	},
 	{
+		title: "Adviency",
+		description: "Lista de regalos de navidad. La idea general del challenge es agregar una feature en los 24 días de duración que el mismo tiene. Cada día, hay que rehacer el proyeco desde cero.",
+		technologies: "Vite, React JS, React Icons, React Snowfall y Chakra UI.",
+		github: "https://github.com/gsabik/adviency",
+		visit: "https://adviency-christmas.vercel.app/",
+	},
+	{
 		title: "OpenWeather API",
 		description: "¿Cómo va a estar el clima hoy o dentro de los próximos días? La verdad no sé, pero si entras acá te vas a enterar.",
 		technologies: "Vite, React JS, React Router, Chakra UI y OpenWeather API.",
